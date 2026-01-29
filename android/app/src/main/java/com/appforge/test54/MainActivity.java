b63c98c868da0749777463eb1c942931a44b9da4
saziye <szy.dag2222@gmail.com>
2026-01-29 21:05:51 +0300

    Initial commit: Generated by AppForge

---
+++ android/app/src/main/java/com/appforge/test54/MainActivity.java
@@ -0,0 +1,24 @@
+package com.appforge.test54;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "test54";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        false
+    );
+  }
+}
